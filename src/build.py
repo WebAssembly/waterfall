bd398cda2f7b07b0abe656cc6c247961d5f1867e
JF Bastien <github@jfbastien.com>
2016-01-23 12:25:28 -0800

    Mark d8 tests as non-flaky
    Recent runs haven't seemed to flake. Let's see if it stays that way.

--- src/build.py
+++ src/build.py
@@ -664,8 +664,7 @@ def main():
       fails=V8_KNOWN_TORTURE_FAILURES,
       extension='wasm',
       has_output=False,
-      wasmjs=WASMJS,
-      is_flaky=True)  # TODO d8 out-of-bounds tests seem flaky.
+      wasmjs=WASMJS)
   # Keep the summary step last: it'll be marked as red if the return code is
   # non-zero. Individual steps are marked as red with StepFail().
   Summary(repos)
