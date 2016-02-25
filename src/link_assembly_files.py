be906ab20f7f19ffe33d52f2ef34cbb98a3b5503
JF Bastien <jfb@chromium.org>
2016-02-25 13:04:21 -0800

    s2wasm: add start

--- src/link_assembly_files.py
+++ src/link_assembly_files.py
@@ -34,7 +34,8 @@ def link(infile, outfile, extras):
   linker = extras['linker']
   basename = os.path.basename(linker)
   commands = {
-      's2wasm': [linker, '--allocate-stack', '1048576', '-o', outfile, infile],
+      's2wasm': [linker, '--allocate-stack', '1048576',
+                 '-o', outfile, infile, '--start'],
       'wasm-as': [linker, '-o', outfile, infile],
   }
   return commands[basename]
