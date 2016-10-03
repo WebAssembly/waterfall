5d1f3ffa4d72214f70d811fea58192f6d601ff65
Sam Clegg <sbc@chromium.org>
2016-10-03 11:17:58 -0700

    Upload lkgr/latest file to per-builder sub-directory (#113)
    * Upload lkgr/latest file to per-builder sub-directory
    
    Otherwise the three different builders will race to
    update these files.
    
    Also, add .json file extension so it clear what type
    of file these are.
    
    Also, and missing newline to end of json file.
    
    * Fix upload path
    
    * Fix name of uploaded file

--- src/build.py
+++ src/build.py
@@ -604,12 +604,13 @@ class Filter:
      selected targets.
   """
   def __init__(self, name=None, include=None, exclude=None):
-    """ include:
-         if present, only items in it will be included (if empty, nothing will
-         be included).
-        exclude:
-         if present, items in it will be excluded.
-        include ane exclude cannot both be present.
+    """
+    include:
+      if present, only items in it will be included (if empty, nothing will
+      be included).
+    exclude:
+      if present, items in it will be excluded.
+      include ane exclude cannot both be present.
     """
     if include and exclude:
       raise Exception('Filter cannot include both include and exclude rules')
@@ -619,7 +620,7 @@ class Filter:
     self.exclude = exclude
 
   def Apply(self, targets):
-    """ Return the filtered list of targets. """
+    """Return the filtered list of targets."""
     all_names = [t.name for t in targets]
     specified_names = self.include or self.exclude or []
     missing_names = [i for i in specified_names if i not in all_names]
@@ -632,7 +633,7 @@ class Filter:
     return [t for t in targets if self.Check(t.name)]
 
   def Check(self, target):
-    """ Return true if the specified target will be run. """
+    """Return true if the specified target will be run."""
     if self.include is not None:
       return target in self.include
 
@@ -641,7 +642,7 @@ class Filter:
     return True
 
   def All(self):
-    """ Return true if all possible targets will be run. """
+    """Return true if all possible targets will be run."""
     return self.include is None and not self.exclude
 
 
@@ -650,8 +651,16 @@ def SyncRepos(filter, sync_lkgr=False):
 
   good_hashes = None
   if sync_lkgr:
-    lkgr_file = os.path.join(WORK_DIR, 'lkgr')
-    cloud.Download('lkgr', lkgr_file)
+    if sys.platform.startswith('linux'):
+      buildername = 'linux'
+    elif sys.platform == 'darwin':
+      buildername = 'mac'
+    elif sys.platform == 'win32':
+      buildername = 'windows'
+    else:
+      raise Exception('Unknown platform: %s' % sys.platform)
+    lkgr_file = os.path.join(WORK_DIR, 'lkgr.json')
+    cloud.Download('%s/lkgr.json' % buildername, lkgr_file)
     lkgr = json.loads(open(lkgr_file).read())
     good_hashes = {}
     for k, v in lkgr['repositories'].iteritems():
@@ -1072,13 +1081,16 @@ def Summary(repos):
   info_file = os.path.join(INSTALL_DIR, 'buildinfo.json')
   with open(info_file, 'w+') as f:
     f.write(info_json)
+    f.write('\n')
 
-  buildbot.Link('latest', cloud.Upload(info_file, 'latest'))
+  latest_file = '%s/%s' % (BUILDBOT_BUILDERNAME, 'latest.json')
+  buildbot.Link('latest.json', cloud.Upload(info_file, latest_file))
 
   if buildbot.Failed():
     buildbot.Fail()
   else:
-    buildbot.Link('lkgr', cloud.Upload(info_file, 'lkgr'))
+    lkgr_file = '%s/%s' % (BUILDBOT_BUILDERNAME, 'lkgr.json')
+    buildbot.Link('lkgr.json', cloud.Upload(info_file, lkgr_file))
 
 
 def AllBuilds(use_asm=False):
