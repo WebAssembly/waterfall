74b4b9b9d30df2c4ec0103a2d0c01c08f30e1df1
Sam Clegg <sbc@chromium.org>
2016-10-04 15:03:46 -0700

    Honor EMCC_WASM_BACKEND in emcc wrapper scripts
    Also, document behaviour in debian package README
    file.

--- src/emcc_wrapper.sh
+++ src/emcc_wrapper.sh
@@ -4,7 +4,16 @@ SCRIPT_FILE=$(readlink -f "${BASH_SOURCE}")
 SCRIPT_DIR=$(cd $(dirname "${SCRIPT_FILE}") && pwd)
 WASM_ROOT=$(dirname "${SCRIPT_DIR}")
 
+# Without this emscripten will attempt to write an '_santiy' config
+# file to the toolchain root, which will fail because its not a
+# writable location.
 export EMCC_SKIP_SANITY_CHECK=1
-export EM_CONFIG="${WASM_ROOT}/emscripten_config"
+
+# Use fastcomp backend unless EMCC_WASM_BACKEND is set to 1
+if [[ $EMCC_WASM_BACKEND == 1 ]]; then
+  export EM_CONFIG="${WASM_ROOT}/emscripten_config_vanilla"
+else
+  export EM_CONFIG="${WASM_ROOT}/emscripten_config"
+fi
 
 exec "${WASM_ROOT}/emscripten/$(basename $0)" $*
