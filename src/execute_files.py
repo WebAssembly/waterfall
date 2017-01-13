c84fe4927a1a9aa5fa26fd94764360c6290dc356
Derek Schuff <dschuff@chromium.org>
2017-01-12 16:17:35 -0800

    Only strip off windows exe suffixes in execute_files (#165)
    wasm.opt is the filename of the spec interpreter, so don't strip off
    all suffixes; just ones that correspond to Windows executables

--- src/execute_files.py
+++ src/execute_files.py
@@ -32,7 +32,8 @@ def create_outname(outdir, infile):
 def execute(infile, outfile, extras):
   """Create the command-line for an execution."""
   runner = extras['runner']
-  basename = os.path.splitext(os.path.basename(runner))[0]
+  # Strip only Windows suffxes (instead of all) because wasm.opt has a suffix
+  basename = os.path.basename(runner).replace('.exe', '').replace('.bat', '')
   out_opt = ['-o', outfile] if outfile else []
   extra_files = extras['extra_files']
   config = basename
