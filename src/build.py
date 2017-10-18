7a3916182e38fba0de97edb890fb1a4f21bc61f6
Sam Clegg <sbc@chromium.org>
2017-10-18 11:11:33 -0700

    Fix invocation of v8 run-tests.py (#254)
    This auto detect feature of run-tests.py was removed
    upstream in:
     https://chromium-review.googlesource.com/721659

--- src/build.py
+++ src/build.py
@@ -898,7 +898,7 @@ def V8():
   proc.check_call(['ninja', '-v', '-C', V8_OUT_DIR, 'd8', 'unittests'] + jobs,
                   cwd=V8_SRC_DIR)
   proc.check_call(['tools/run-tests.py', 'unittests', '--no-presubmit',
-                   '--shell-dir', V8_OUT_DIR],
+                   '--outdir', V8_OUT_DIR],
                   cwd=V8_SRC_DIR)
   # Copy the V8 blobs as well as the ICU data file for timezone data.
   # icudtl.dat is the little-endian version, which goes with x64.
