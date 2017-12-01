8ea7ac5d8d4cbebbfb59105cbc05422ab49d0ef0
Derek Schuff <dschuff@chromium.org>
2017-12-01 10:58:25 -0800

    fix ar and ranlib in cmake toolchain file

--- src/build.py
+++ src/build.py
@@ -1169,8 +1169,8 @@ def CompilerRT():
              os.path.join(INSTALL_DIR, 'wasm_standalone.cmake'),
              '-DCMAKE_C_COMPILER_WORKS=ON',
              # TODO: why doesn't setting CMAKE_AR in the toolchain file work?
-             '-DCMAKE_AR=' + os.path.join(INSTALL_BIN, 'llvm-ar'),
-             '-DCMAKE_RANLIB=' + os.path.join(INSTALL_BIN, 'llvm-ranlib'),
+             #'-DCMAKE_AR=' + os.path.join(INSTALL_BIN, 'llvm-ar'),
+             #'-DCMAKE_RANLIB=' + os.path.join(INSTALL_BIN, 'llvm-ranlib'),
              '-DCOMPILER_RT_BAREMETAL_BUILD=On',
              '-DCOMPILER_RT_BUILD_XRAY=OFF',
              '-DCOMPILER_RT_INCLUDE_TESTS=OFF',
