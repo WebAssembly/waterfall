ac89ab35559153a420d032880cbb7431f4df1ce2
Derek Schuff <dschuff@chromium.org>
2017-12-01 11:30:45 -0800

    Update invocation of host_toolchain.py on windows (#294)
    It grew a new required command-line flag in
    https://chromium-review.googlesource.com/768549, so give it the value 0
    (which means don't disable goma).

--- src/host_toolchains.py
+++ src/host_toolchains.py
@@ -82,7 +82,7 @@ def SetUpVSEnv(outdir):
   # block
   runtime_dirs = os.pathsep.join(paths['runtime_dirs'])
   proc.check_call([SETUP_TOOLCHAIN,
-                   'foo', paths['win_sdk'], runtime_dirs, 'x64'],
+                   'foo', paths['win_sdk'], runtime_dirs, 'x64', '0'],
                   cwd=outdir)
   return GetVSEnv(outdir)
 
