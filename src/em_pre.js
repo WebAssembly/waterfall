da797135d1e8785c2b63f693086876fd8fbc37cc
Jacob Gravelle <jgravelle@google.com>
2018-03-16 14:45:14 -0700

    More precise comment

--- src/em_pre.js
+++ src/em_pre.js
@@ -1,9 +1,11 @@
 var Module = {}
 
 Module['onAbort'] = function(reason) {
-  // d8 does not exit with status 1 when a promise throws. Emscripten calls
-  // abort when a wasm module fails to initialize, which is implemented in JS
-  // as a function that terminates execution by throwing an exception.
+  // JS shells do not exit with status 1 when a promise is rejected. Emscripten
+  // calls abort when a wasm module fails to initialize, which is implemented in
+  // JS as a function that terminates execution by throwing an exception, which
+  // causes the instantiate promise to be rejected, which causes the shell to
+  // falsely return 0.
   // Emscripten's abort has an 'onAbort' hook, so we can use that to call d8's
   // quit, which correctly returns an error code even from a promise.
   quit(1);
