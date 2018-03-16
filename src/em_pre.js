9da3a2b1dc1692b6bb0ec0bf73b187452f0d5c2f
Jacob Gravelle <jgravelle@google.com>
2018-03-16 14:26:31 -0700

    Typo: promis->promise

--- src/em_pre.js
+++ src/em_pre.js
@@ -5,6 +5,6 @@ Module['onAbort'] = function(reason) {
   // abort when a wasm module fails to initialize, which is implemented in JS
   // as a function that terminates execution by throwing an exception.
   // Emscripten's abort has an 'onAbort' hook, so we can use that to call d8's
-  // quit, which correctly returns an error code even from a promis.
+  // quit, which correctly returns an error code even from a promise.
   quit(1);
 };
