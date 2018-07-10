56c11b9ffeb8e8e0f238e6e042276d07b2e4d5b6
Sergiy Byelozyorov <rryk.ua@gmail.com>
2018-07-10 11:31:08 +0200

    Add remaining LUCI configs (#379)
    * Add remaining LUCI configs
    
    * Add luci-logdog.cfg
    
    * Added Windows version to the os dimension

---
+++ PRESUBMIT.py
@@ -0,0 +1,25 @@
+# Copyright 2018 the V8 project authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Presubmit script for changes in the infrastructure configs."""
+
+
+def _CommonChecks(input_api, output_api):
+  """Checks common to both upload and commit."""
+  results = []
+  results.extend(
+      input_api.canned_checks.CheckChangedLUCIConfigs(input_api, output_api))
+  return results
+
+
+def CheckChangeOnUpload(input_api, output_api):
+  results = []
+  results.extend(_CommonChecks(input_api, output_api))
+  return results
+
+
+def CheckChangeOnCommit(input_api, output_api):
+  results = []
+  results.extend(_CommonChecks(input_api, output_api))
+  return results
