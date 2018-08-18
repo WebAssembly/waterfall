2dfe522aca11bccee363bb636e6ba6ee7814b9a9
Derek Schuff <dschuff@chromium.org>
2018-08-17 17:39:16 -0700

    Build the native asmjs optimizer in a separate step (#402)
    The way embuilder builds the native optimizer with cmake doesn't work on the waterfall on Windows; and in any case it's better to force the optimizer binary anyway, to avoid building it on-demand.

--- src/build.py
+++ src/build.py
@@ -1125,6 +1125,20 @@ def Emscripten():
                   # Ignore the big git blob so it doesn't get archived.
                   ignore=shutil.ignore_patterns('.git'))
 
+  # Manually build the native asm.js optimizer (the cmake build in embuilder
+  # doesn't work on the waterfall)
+  optimizer_out_dir = os.path.join(WORK_DIR, 'em-optimizer-out')
+  Mkdir(optimizer_out_dir)
+  cc_env = BuildEnv(optimizer_out_dir)
+  command = [PREBUILT_CMAKE_BIN, '-G', 'Ninja',
+             '-DCMAKE_BUILD_TYPE=Release',
+             os.path.join(EMSCRIPTEN_SRC_DIR, 'tools', 'optimizer')
+             ] + OverrideCMakeCompiler()
+  proc.check_call(command, cwd=optimizer_out_dir, env=cc_env)
+  proc.check_call(['ninja'] + host_toolchains.NinjaJobs(),
+                  cwd=optimizer_out_dir, env=cc_env)
+  CopyBinaryToArchive(Executable(os.path.join(optimizer_out_dir, 'optimizer')))
+
   def WriteEmscriptenConfig(infile, outfile):
     with open(infile) as config:
       text = config.read().replace('{{WASM_INSTALL}}',
