d405d82f21ce822ff8db1208330c17f49f262d85
Sam Clegg <sbc@chromium.org>
2019-04-30 16:04:31 -0700

    Switch from jfb's musl port to wasi-sysroot (#492)

--- src/compile_torture_tests.py
+++ src/compile_torture_tests.py
@@ -69,7 +69,7 @@ def run(cc, cxx, testsuite, sysroot_dir, fails, exclusions, out, config, opt):
   script_dir = os.path.dirname(os.path.abspath(__file__))
   pre_js = os.path.join(script_dir, 'em_pre.js')
 
-  cflags_common = ['-DSTACK_SIZE=524288',
+  cflags_common = ['-DSTACK_SIZE=524288', '-D_WASI_EMULATED_MMAN',
                    '-w', '-Wno-implicit-function-declaration', '-' + opt]
   cflags_c = ['--std=gnu89']
   cflags_cxx = []
