31181baa90ab265c3820f843a53e8776f5798df7
Sam Clegg <sbc@chromium.org>
2019-07-01 10:13:24 -0700

    Fix llvm build on older (10.12 and below) OSX versions
    See https://github.com/emscripten-core/emscripten/issues/5418

--- src/build.py
+++ src/build.py
@@ -815,6 +815,11 @@ def LLVM():
       '-DLLVM_ENABLE_TERMINFO=%d' % (not IsLinux()),
   ])
 
+  if IsMac():
+    # Workaround for https://github.com/kripken/emscripten/issues/5418.
+    # Should we drop support for OS X 10.12 and below?
+    command.append('-DHAVE_FUTIMENS=0')
+
   jobs = host_toolchains.NinjaJobs()
 
   proc.check_call(command, cwd=build_dir, env=cc_env)
