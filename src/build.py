0e109d46e5cf14f523fec0d2f622ec6f13b4970a
Derek Schuff <dschuff@chromium.org>
2019-11-13 13:19:42 -0800

    Bump clobber version
    This clobber probably should have gone in with the compiler change for wabt.

--- src/build.py
+++ src/build.py
@@ -82,7 +82,7 @@ LLVM_VERSION = '10.0.0'
 # Update this number each time you want to create a clobber build.  If the
 # clobber_version.txt file in the build dir doesn't match we remove ALL work
 # dirs.  This works like a simpler version of chromium's landmine feature.
-CLOBBER_BUILD_TAG = 12
+CLOBBER_BUILD_TAG = 13
 
 options = None
 
