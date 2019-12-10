f46b37bec2771d6d09a1a70d6f4a7eb71e58e00e
Derek Schuff <dschuff@chromium.org>
2019-12-10 12:37:08 -0800

    fix flake8

--- src/host_toolchains.py
+++ src/host_toolchains.py
@@ -95,8 +95,8 @@ def SetUpVSEnv(outdir):
   # block
   runtime_dirs = os.pathsep.join(paths['runtime_dirs'])
   proc.check_call(SetupToolchain() + [
-                   'foo', paths['win_sdk'], runtime_dirs,
-                   'win', 'x64', 'environment.x64'],
+    'foo', paths['win_sdk'], runtime_dirs,
+    'win', 'x64', 'environment.x64'],
                   cwd=outdir)
   return GetVSEnv(outdir)
 
