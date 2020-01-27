ce5ffba796b4fd48b824337e081f78ccc161bdf3
Sam Clegg <sbc@chromium.org>
2020-01-27 15:39:13 -0800

    revert part

--- src/build.py
+++ src/build.py
@@ -945,11 +945,11 @@ def Jsvu():
     os_id = 'linux64'
     js_engines = 'javascriptcore'
 
-  npm = Executable(os.path.join(NodeBinDir(), 'npm'))
   try:
     # https://github.com/GoogleChromeLabs/jsvu#installation
     # ...except we install it locally instead of globally.
-    proc.check_call([npm, 'install', 'jsvu'], cwd=jsvu_dir)
+    proc.check_call([os.path.join(NodeBinDir(), 'npm'), 'install', 'jsvu'],
+                    cwd=jsvu_dir)
 
     jsvu_bin = Executable(os.path.join(
         jsvu_dir, 'node_modules', 'jsvu', 'cli.js'))
@@ -1610,7 +1610,7 @@ def ExecuteEmscriptenTestSuite(name, tests, config, outdir, warn_only=False):
   buildbot.Step('Execute emscripten testsuite (%s)' % name)
   Mkdir(outdir)
   try:
-    proc.check_call([Executable(os.path.join(NodeBinDir(), 'npm')), 'install'],
+    proc.check_call([os.path.join(NodeBinDir(), 'npm'), 'install'],
                     cwd=GetInstallDir('emscripten'))
     proc.check_call(
         [GetInstallDir('emscripten', 'tests', 'runner.py'),
